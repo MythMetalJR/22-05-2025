7777f1e8e3fdae460806b8c64aceb08b5f506a73
Josue Rodriguez <wilsonrodri3000@gmail.com>
2025-05-22 18:09:56 -0500

    Add files via upload

---
+++ JosueRExamenmediociclo.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.josuerexamenmediociclo;
+
+public class JosueRExamenmediociclo {
+
+    public static void main(String[] args) {
+        System.out.println("--------------Bienvenido-------------\n");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
